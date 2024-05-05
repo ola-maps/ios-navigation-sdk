a4d057d1cffca7905e0a8a163305357eaf031e59
Abhishek Ravi 🌵 <ab.abhishek.ravi@gmail.com>
2024-05-05 23:45:45 +0530

    Fixed Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,37 @@
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "YourFramework",
+    name: "OlaMapNavigation",
     platforms: [
-        .iOS(.v12),   // Adjust the version as needed
-        .macOS(.v10_12),
-        // Add other platforms (watchOS, tvOS) as needed
+        .iOS(.v13)
     ],
     products: [
-        .library(name: "YourFramework", targets: ["YourFramework"]),
+        .library(name: "OlaMapNavigation", targets: ["OlaMapNavigation"]),
+    ],
+    dependencies: [
+        .package(url: " https://github.com/moengage/MoEngage-iOS-SDK.git", from: "9.16.2"),
     ],
     targets: [
         .binaryTarget(
-            name: "YourFramework",
-            url: "https://example.com/path/to/YourFramework.xcframework.zip", // Replace with the actual URL
-            checksum: "sha256:yourchecksum"  // Replace with the actual checksum
+            name: "OlaMapNavigationSDK",
+            path: "OlaMapNavigationSDK.xcframework",
         ),
+        .binaryTarget(
+            name: "OlaMapDirection",
+            path: "OlaMapDirection.xcframework",
+        ),
+        .binaryTarget(
+            name: "MapLibre",
+            path: "MapLibre.xcframework",
+        ),
+        .binaryTarget(
+            name: "OlaTurf",
+            path: "OlaTurf.xcframework",
+        ),
+        .binaryTarget(
+            name: "OlaTurf",
+            path: "OlaTurf.xcframework",
+        )
     ]
 )
