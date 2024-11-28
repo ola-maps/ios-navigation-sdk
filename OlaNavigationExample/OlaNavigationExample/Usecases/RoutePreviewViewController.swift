5cbc9ad7199d89d873aa756ad54be19aedfd5de9
OlaMap Team <admin@olamaps.io>
2024-11-28 21:08:13 +0530

    Added Release 1.6.86

--- OlaNavigationExample/OlaNavigationExample/Usecases/RoutePreviewViewController.swift
+++ OlaNavigationExample/OlaNavigationExample/Usecases/RoutePreviewViewController.swift
@@ -10,14 +10,16 @@ import OlaMapNavigation
 
 class RoutePreviewViewController: BaseViewController {
 
-    private let olaMap = OlaMapService(auth: .apiKey(key: AppUtility.getAPIKey()), tileURL: URL(string: "https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json")!, env: .production, projectId: "Ola-Navigation")
+    private let olaMap = OlaMapService(auth: .apiKey(key: AppUtility.getAPIKey()), tileURL: URL(string: "https://api.olamaps.io/tiles/vector/v1/styles/default-light-standard/style.json")!)
 
     private let destinationCoordinate = OlaCoordinate(latitude: 12.923086586316643, longitude: 77.63758819612909)
     private let originCoordinate = OlaCoordinate(latitude: 12.9698, longitude: 77.7500)
     
     let popupDecoratedModel = PopupDecoratedModel(font: .systemFont(ofSize: 14), labelActiveColor: .red, labelInactiveColor: .white, activeBackgroudColor: .white, inactiveBackgroundColor: .gray, height: 32, cornerRadius: 4)
+    let polylineDecorator = PolylineDecorator(strokeColor: .black, strokWidth: 7.0, strokeAlpha: 0.5)
+    let edges = UIEdgeInsets(top: 100, left: 30, bottom: 50, right: 30)
     
-    lazy var directionResponseData = self.loadMockJSON("direction_one")
+    lazy var directionResponseData = self.loadMockJSON("direction_two")
     
     private lazy var destinationMarker: OlaAnnotationView = {
         let annotationView =  CustomAnnotationView(identifier: "destination-marker", image: UIImage(named: "img_drop"))
@@ -29,7 +31,6 @@ class RoutePreviewViewController: BaseViewController {
         super.viewDidLoad()
         view.backgroundColor = .darkGray
         self.initializeMap()
-        self.createPreview(self.originCoordinate, self.destinationCoordinate)
         if let image = UIImage(named: "img_compass") {
             olaMap.setCustomCompassImage(image)
         }
@@ -40,6 +41,7 @@ class RoutePreviewViewController: BaseViewController {
         olaMap.addCurrentLocationButton(self.view)
         olaMap.setCurrentLocationMarkerColor(UIColor.systemGreen)
         olaMap.delegate = self
+        olaMap.setDebugLogs(true)
         olaMap.setMaxZoomLevel(16.0)
         olaMap.showCompassVisibility = true
         olaMap.setRotatingGesture(true)
@@ -48,23 +50,17 @@ class RoutePreviewViewController: BaseViewController {
     // Draw Route Preview
     func createPreview(_ origin: OlaCoordinate, _ destination: OlaCoordinate) {
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: { [weak self] in
-            if let self = self {
-             
-                if let previewData = self.directionResponseData {
-                    
-                    self.olaMap.showRoutePreview(jsonData: previewData,
-                                                 popupDecorator: self.popupDecoratedModel,
-                                                 isTrafficSupported: true,
-                                                 isEtaBadge: true,
-                                                 edgePadding: UIEdgeInsets(top: 30, left: 10, bottom: 30, right: 10),
-                                                 stops: nil)
-                    self.addMarkers()
-                }
-                
-            }
+        if let previewData = self.directionResponseData {
+            self.olaMap.showRoutePreview(jsonData: previewData,
+                                         popupDecorator: self.popupDecoratedModel,
+                                         polylineDecorator: self.polylineDecorator,
+                                         isTrafficSupported: true,
+                                         isEtaBadge: true,
+                                         edgePadding: edges, stops: [])
             
-        })
+            self.addMarkers()
+        }
+    
     }
     
     func addMarkers() {
@@ -81,6 +77,9 @@ extension RoutePreviewViewController: OlaMapServiceDelegate {
         
     }
     
+    func mapSuccessfullyLoaded() {
+        self.createPreview(self.originCoordinate, self.destinationCoordinate)
+    }
     
     func didChangeCamera() {
         //TODO:
